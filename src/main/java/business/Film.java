4e63035958f67d12087f6c74bd7a7ee2188bcd68
playerzer0-ui <playerzero745@gmail.com>
2024-03-21 15:48:53 +0000

    START

---
+++ src/main/java/business/Film.java
@@ -0,0 +1,44 @@
+package business;
+
+import java.util.ArrayList;
+
+public class Film {
+    private String title;
+    private ArrayList<String> Genre;
+    private double ratings;
+    private int rateCount;
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<String> getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(ArrayList<String> genre) {
+        Genre = genre;
+    }
+
+    public double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(double ratings) {
+        this.ratings = ratings;
+    }
+
+    public int getRateCount() {
+        return rateCount;
+    }
+
+    public void setRateCount(int rateCount) {
+        this.rateCount = rateCount;
+    }
+}
